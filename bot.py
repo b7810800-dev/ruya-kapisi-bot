d8e6a6730dc50d62f7ece16c443dd68e94aa8657
b7810800-dev <b7810800@gmail.com>
2025-11-24 22:21:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ bot_token = 7972292056:AAE9ZXjJIrS5xJsoroJ_LPf6gbTLRMermjk
 # ------------------------------------------------------------
 
 # Botu başlat
-client = TelegramClient('ruyakapisi_session', api_id, api_hash).start(bot_token=bot_token)
+client = TelegramClient('ruyakapisi_session
 
 @client.on(events.NewMessage(pattern='/start'))
 async def start(event):
