7468eeb3648d63e6c3bf80c492b62037db04b843
b7810800-dev <b7810800@gmail.com>
2025-11-25 14:59:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,13 +25,6 @@ async def tum_mesajlar(event):
 
 print("🌙 Rüya Kapısı botu çalışıyor... 7/24 online!")
 print("Rüya Kapısı botu çalışıyor... 7/24 online! ❤️")
-client.run_until_disconnected()
-
-async def main():
-    await client.start(bot_token=bot_token)
-    print("Rüya Kapısı botu çalışıyor... 7/24 online! ❤️")
-    await client.run_until_disconnected()
-asyncio.run(main())
 async def main():
     await client.start(bot_token=bot_token)
     print("Rüya Kapısı botu çalışıyor... 7/24 online! ❤️")
