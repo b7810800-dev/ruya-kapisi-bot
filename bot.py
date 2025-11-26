8c7107bd99870c461eb93f12bdad805393cff9a8
b7810800-dev <b7810800@gmail.com>
2025-11-26 22:03:42 +0300

    Update bot.py
    Final Fix

--- bot.py
+++ bot.py
@@ -19,9 +19,8 @@ async def start(event):
 @client.on(events.NewMessage)
 async def tum_mesajlar(event):
     if event.is_private:  # sadece özel mesajlarda çalışsın
-        ruya = event.raw_text.strip()
-    if '/start' in ruya.lower():
-    return
+ruya = event.raw_text.strip()
+    if '/start' in ruya.lower(): return
             await event.reply("Rüyanı aldım, bir saniye yorumluyorum... 🌟")
 async def main():
     await client.start(bot_token=bot_token)
